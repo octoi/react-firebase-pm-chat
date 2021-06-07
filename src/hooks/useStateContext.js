0ca956bd63356c47cbb03f029bbfba308db49a00
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-07 17:33:35 +0530

    removed context setup

--- src/hooks/useStateContext.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { AppContextState } from '../wrappers/Context';
-
-export default function useStateContext() {
-    return useContext(AppContextState);
-}
