24598e7abffb943f95a71c2d4e725e0bb5a639e4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-07 17:27:26 +0530

    used header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { Container } from '@chakra-ui/react';
 import React from 'react';
 import ChakraWrap from './wrappers/ChakraWrap';
-import { Container } from '@chakra-ui/react';
+import Header from './components/Header';
 
 function App() {
     return (
         <ChakraWrap>
             <Container maxW="container.xl" mt={10}>
+                <Header />
             </Container>
         </ChakraWrap>
     );
