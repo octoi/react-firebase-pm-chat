64de64eac2ac2509f5c4dee462af87806329d51c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-07 17:43:48 +0530

    auth setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { Container } from '@chakra-ui/react';
 import React from 'react';
 import ChakraWrap from './wrappers/ChakraWrap';
 import Header from './components/Header';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase/firebase';
+import { Container } from '@chakra-ui/react';
 
 function App() {
+
+    const [user, loading, error] = useAuthState(auth);
+
     return (
         <ChakraWrap>
             <Container maxW="container.xl" mt={10}>
-                <Header />
+                <Header user={user} />
             </Container>
         </ChakraWrap>
     );
