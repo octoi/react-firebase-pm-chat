caef30f91d0f48677b8e8ada2bad2554b596a4bc
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-08 14:32:46 +0530

    ordering and displaying users data

--- src/components/ChatProfiles.jsx
+++ src/components/ChatProfiles.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Flex, Avatar, Text, Divider } from '@chakra-ui/react';
+import { firestore } from '../firebase/firebase';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 export default function ChatProfiles({ setCurrentChat }) {
+    const usersRef = firestore.collection('users');
+    const query = usersRef.limit(25);
+    const [users] = useCollectionData(query, { idField: 'id' });
+
     return (
-        <div>
-            <ChatProfileCard />
+        <div style={{ marginTop: "50px" }}>
+            {users && users.map(user => <ChatProfileCard user={user} key={user?.id} />)}
         </div>
     )
 }
 
 
-function ChatProfileCard() {
+function ChatProfileCard({ user }) {
     return (
         <div>
             <Flex cursor="pointer" alignItems="center" mb={5} mt={5}>
-                <Avatar src="https://bit.ly/ryan-florence" size="lg" />
+                <Avatar src={user.profile} size="lg" />
                 <Flex direction="column" ml={3}>
-                    <Text fontSize="lg" fontWeight="semibold">Username</Text>
-                    <Text fontSize="md">user@email.com</Text>
+                    <Text fontSize="lg" fontWeight="semibold">{user.name}</Text>
+                    <Text fontSize="md">{user.email}</Text>
                 </Flex>
             </Flex>
             <Divider />
