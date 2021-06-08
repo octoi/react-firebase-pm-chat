46c18438abb2b6cf875886789ee8d8a0f0141bb0
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-08 15:09:50 +0530

    filtering current user

--- src/components/ChatProfiles.jsx
+++ src/components/ChatProfiles.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Flex, Avatar, Text, Divider } from '@chakra-ui/react';
-import { firestore } from '../firebase/firebase';
+import { firestore, auth } from '../firebase/firebase';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 export default function ChatProfiles({ setCurrentChat }) {
@@ -17,6 +17,15 @@ export default function ChatProfiles({ setCurrentChat }) {
 
 
 function ChatProfileCard({ user, onClick }) {
+
+    const currentUser = {
+        name: auth?.currentUser.displayName,
+        email: auth?.currentUser.email,
+        profile: auth?.currentUser.photoURL,
+    }
+
+    if (currentUser.email === user?.email) return null;
+
     return (
         <div>
             <Flex onClick={onClick} cursor="pointer" alignItems="center" mb={5} mt={5}>
@@ -29,4 +38,5 @@ function ChatProfileCard({ user, onClick }) {
             <Divider />
         </div>
     );
+
 }
