50bae95cbf181624e59a3b2e1ec0076b4bfb26da
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-08 14:50:48 +0530

    set current chat

--- src/components/ChatProfiles.jsx
+++ src/components/ChatProfiles.jsx
@@ -5,21 +5,21 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 export default function ChatProfiles({ setCurrentChat }) {
     const usersRef = firestore.collection('users');
-    const query = usersRef.limit(25);
+    const query = usersRef.orderBy('name').limit(10);
     const [users] = useCollectionData(query, { idField: 'id' });
 
     return (
         <div style={{ marginTop: "50px" }}>
-            {users && users.map(user => <ChatProfileCard user={user} key={user?.id} />)}
+            {users && users.map(user => <ChatProfileCard onClick={() => setCurrentChat(user.id)} user={user} key={user?.id} />)}
         </div>
     )
 }
 
 
-function ChatProfileCard({ user }) {
+function ChatProfileCard({ user, onClick }) {
     return (
         <div>
-            <Flex cursor="pointer" alignItems="center" mb={5} mt={5}>
+            <Flex onClick={onClick} cursor="pointer" alignItems="center" mb={5} mt={5}>
                 <Avatar src={user.profile} size="lg" />
                 <Flex direction="column" ml={3}>
                     <Text fontSize="lg" fontWeight="semibold">{user.name}</Text>
