f34a9a315125ae3edbac143f38c41a366becc484
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-08 15:13:23 +0530

    added bread crump

--- src/components/PmChat.jsx
+++ src/components/PmChat.jsx
@@ -1,22 +1,30 @@
 import { useState } from 'react';
 import { Input, Flex, Button } from '@chakra-ui/react';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react"
 
-export default function PmChat() {
+export default function PmChat({ currentChat }) {
     const [formValue, setFormValue] = useState('');
 
+
     return (
-        <Flex direction="column" justifyContent="space-between" marginTop="50px">
-            <form style={{ display: "flex" }}>
-                <Input
-                    variant="filled"
-                    placeholder="say something nice"
-                    style={{ marginRight: "10px" }}
-                    value={formValue}
-                    onChange={(e) => setFormValue(e.target.value)}
-                />
-                <Button type="submit" disabled={!formValue}>🕊️</Button>
-            </form>
-        </Flex>
+        <div>
+            <Breadcrumb>
+                <BreadcrumbItem><BreadcrumbLink href="/">Chats</BreadcrumbLink></BreadcrumbItem>
+                <BreadcrumbItem><BreadcrumbLink href="#">{currentChat.name}</BreadcrumbLink></BreadcrumbItem>
+            </Breadcrumb>
+            <Flex direction="column" justifyContent="space-between" marginTop="50px">
+                <form style={{ display: "flex" }}>
+                    <Input
+                        variant="filled"
+                        placeholder="say something nice"
+                        style={{ marginRight: "10px" }}
+                        value={formValue}
+                        onChange={(e) => setFormValue(e.target.value)}
+                    />
+                    <Button type="submit" disabled={!formValue}>🕊️</Button>
+                </form>
+            </Flex>
+        </div>
     )
 }
 
