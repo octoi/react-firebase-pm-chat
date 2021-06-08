91cae3259676f07a08780490805b92d9b475a5a9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-08 14:05:34 +0530

    chat profiles

---
+++ src/components/ChatProfiles.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Flex, Avatar, Text, Divider } from '@chakra-ui/react';
+
+export default function ChatProfiles() {
+    return (
+        <div>
+            <ChatProfileCard />
+        </div>
+    )
+}
+
+
+function ChatProfileCard() {
+    return (
+        <div>
+            <Flex cursor="pointer" alignItems="center" mb={5} mt={5}>
+                <Avatar src="https://bit.ly/ryan-florence" size="lg" />
+                <Flex direction="column" ml={3}>
+                    <Text fontSize="lg" fontWeight="semibold">Username</Text>
+                    <Text fontSize="md">user@email.com</Text>
+                </Flex>
+            </Flex>
+            <Divider />
+        </div>
+    );
+}
