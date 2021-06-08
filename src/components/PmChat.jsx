144fa995d2dc471027befb910351fe69946bcdd9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-08 14:55:37 +0530

    basic form

--- src/components/PmChat.jsx
+++ src/components/PmChat.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import { useState } from 'react';
+import { Input, Flex, Button } from '@chakra-ui/react';
 
 export default function PmChat() {
+    const [formValue, setFormValue] = useState('');
+
     return (
-        <div>
-            pm chat
-        </div>
+        <Flex mt={10} direction="column" justifyContent="space-between" marginTop="20px">
+            <form style={{ display: "flex" }}>
+                <Input
+                    variant="filled"
+                    placeholder="say something nice"
+                    style={{ marginRight: "10px" }}
+                    value={formValue}
+                    onChange={(e) => setFormValue(e.target.value)}
+                />
+                <Button type="submit" disabled={!formValue}>🕊️</Button>
+            </form>
+        </Flex>
     )
 }
+
