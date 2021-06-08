b92380f5d2bfa5b445b97b6889a5dae3b495e05d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-08 15:43:11 +0530

    stoping project, stackoverflow is down

--- src/components/PmChat.jsx
+++ src/components/PmChat.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { auth, firestore } from '../firebase/firebase';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
 import { Input, Flex, Button } from '@chakra-ui/react';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react"
 
 export default function PmChat({ currentChat }) {
+    const messageRef = firestore.collection('chats');
+    const [chatRoom, setChatRoom] = useState();
+
     const user = {
         name: auth?.currentUser.displayName,
         email: auth?.currentUser.email,
         profile: auth?.currentUser.photoURL,
     }
+
+    useEffect(() => {
+        const getChatRoom = async () => {
+            const query = await messageRef
+                .where('user1', 'in', [currentChat.email, user.email])
+                .where('user2', 'not-in', [currentChat.email, user.email])
+                .get();
+            if (query.docs.length === 0) await messageRef.add({
+                user1: user.email,
+                user2: currentChat?.email,
+                messages: []
+            }).then(data => {
+                console.log(data);
+            });
+        }
+        getChatRoom();
+    }, []);
+
+
 
     return (
         <div>
