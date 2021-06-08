2a44fde4dd7c039b62e944092a37087b240564bb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-08 15:16:13 +0530

    got user data and extracted component

--- src/components/PmChat.jsx
+++ src/components/PmChat.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
+import { auth, firestore } from '../firebase/firebase';
 import { Input, Flex, Button } from '@chakra-ui/react';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react"
 
 export default function PmChat({ currentChat }) {
-    const [formValue, setFormValue] = useState('');
-
+    const user = {
+        name: auth?.currentUser.displayName,
+        email: auth?.currentUser.email,
+        profile: auth?.currentUser.photoURL,
+    }
 
     return (
         <div>
@@ -13,18 +17,26 @@ export default function PmChat({ currentChat }) {
                 <BreadcrumbItem><BreadcrumbLink href="#">{currentChat.name}</BreadcrumbLink></BreadcrumbItem>
             </Breadcrumb>
             <Flex direction="column" justifyContent="space-between" marginTop="50px">
-                <form style={{ display: "flex" }}>
-                    <Input
-                        variant="filled"
-                        placeholder="say something nice"
-                        style={{ marginRight: "10px" }}
-                        value={formValue}
-                        onChange={(e) => setFormValue(e.target.value)}
-                    />
-                    <Button type="submit" disabled={!formValue}>🕊️</Button>
-                </form>
+                <ChatForm />
             </Flex>
         </div>
     )
+}
+
+const ChatForm = () => {
+    const [formValue, setFormValue] = useState('');
+
+    return (
+        <form style={{ display: "flex" }}>
+            <Input
+                variant="filled"
+                placeholder="say something nice"
+                style={{ marginRight: "10px" }}
+                value={formValue}
+                onChange={(e) => setFormValue(e.target.value)}
+            />
+            <Button type="submit" disabled={!formValue}>🕊️</Button>
+        </form>
+    );
 }
 
